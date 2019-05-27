d9278f81a1cc45af93e6e341a6479f487c1e0806
Yonnys Quintero <leafar.qr@gmail.com>
2019-05-27 14:12:43 -0400

    loop hottest ready

--- wp-content/themes/Rubbex/partials/hosttest.php
+++ wp-content/themes/Rubbex/partials/hosttest.php
@@ -11,172 +11,48 @@
                 </div>
                 <div class="slider slider-for">
                     <!-- slider -->
+                     <?php $args = array( 'post_type' => 'product', 'posts_per_page' => 4); ?>
+                         <?php $loop = new WP_Query( $args ); ?>
+                         <?php while ( $loop->have_posts() ) : $loop->the_post(); if ( get_field( 'hottest' ) == 1 ):global $product;  ?>
                     <div class="content-best">
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="title-best animated wow fadInDown" data-wow-duration="2s">
                                     <div class="title-best ">
-                                        <h3 class="">Hi I'am Natasha!</h3>
-                                        <span>Face of angel, curves of venus</span>
-                                        <p>Hi there! y nme is Natsha, as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
+                                        <h3 class=""><?php the_field( 'greeting' ); ?></h3>
+                                        <span><?php the_field( 'motto' ); ?></span>
+                                        <p><?php the_field( 'biography' ); ?></p>
                                         <div class="btns animated wow fadeInUp " data-wow-duration="5s">
-                                            <a class="btn-1" href="#">see more</a>
+                                            <a class="btn-1" href="<?php the_permalink(); ?>">see more</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                           
                             <div class="col-md-6 animated wow fadeInRight " data-wow-duration="3s">
 
-                                <div class="img-best ml-md-auto" style="background:url(https://images-eu.ssl-images-amazon.com/images/I/61VAMjUPyhL.jpg)"> </div>
+                                <div class="img-best ml-md-auto" style="background:url(<?php echo get_the_post_thumbnail_url(); ?>)"> </div>
 
                             </div>
                         </div>
                     </div>
-                    <!-- endslider -->
-                    <!-- slider -->
-                    <div class="content-best ">
-                        <div class="row ">
-                            <div class="col-md-6">
-                                <div class="title-best">
-                                    <h3>Hi  I'am Star!</h3>
-                                    <span>Face of angel, curves of venus</span>
-                                    <p>Hi there! y nme is Natsha, as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
-                                    <div class="btns animated wow fadeInUp " data-wow-duration="5s">
-                                        <a class="btn-1" href="#">see more</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="img-best ml-md-auto" style="background:url(https://files.merca20.com/uploads/2017/03/Lumidoll.png)"> </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- endslider -->
-
-                    <!-- slider -->
-                    <div class="content-best ">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="title-best">
-                                    <div class="title-best">
-                                        <h3>Hi I'm Tessa!</h3>
-                                        <span>Face of angel, curves of venus</span>
-                                        <p>Hi there! y nme is Natsha, as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
-                                        <div class="btns animated wow fadeInUp " data-wow-duration="5s">
-                                            <a class="btn-1" href="#">see more</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="img-best ml-md-auto" style="background:url(https://eldiariony.files.wordpress.com/2017/03/aki-dolls1a1.jpg?quality=80&strip=all&strip=all)"> </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- endslider -->
-                    <!-- slider -->
-                    <div class="content-best">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="title-best">
-                                    <div class="title-best">
-                                        <h3>Hi  I'am Lilian!</h3>
-                                        <span>Face of angel, curves of venus</span>
-                                        <p>Hi there! y nme is Natsha, as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
-                                        <div class="btns animated wow fadeInUp " data-wow-duration="5s">
-                                            <a class="btn-1" href="#">see more</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="img-best ml-md-auto" style="background:url(https://images-na.ssl-images-amazon.com/images/I/51fGn99YTOL._SY550_.jpg)"> </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- endslider -->
-                    <!-- slider -->
-                    <div class="content-best">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="title-best">
-                                    <div class="title-best">
-                                        <h3>Hi I'am Carol!</h3>
-                                        <span>Face of angel, curves of venus</span>
-                                        <p>Hi there! y nme is Natsha, as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
-                                        <div class="btns animated wow fadeInUp " data-wow-duration="5s">
-                                            <a class="btn-1" href="#">see more</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="img-best ml-md-auto" style="background:url(https://eldiariony.files.wordpress.com/2017/03/aki-dolls1a1.jpg?quality=80&strip=all&strip=all)"> </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- endslider -->
-                    <div class="content-best">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="title-best">
-                                    <div class="title-best">
-                                        <h3>Hi I'm Lilian!</h3>
-                                        <span>Face of angel, curves of venus</span>
-                                        <p>Hi there! y nme is Natsha, as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
-                                        <div class="btns animated wow fadeInUp " data-wow-duration="5s">
-                                            <a class="btn-1" href="#">see more</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="img-best ml-md-auto" style="background:url(https://images-na.ssl-images-amazon.com/images/I/51fGn99YTOL._SY550_.jpg)"> </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- endslider -->
-
+                    <?php endif; endwhile; ?>
+                    
 
                 </div>
                 <div class="slider slider-nav animated wow fadeInLeft" data-wow-duration="3.5s">
                     <!-- miiniaturas -->
+                       <?php $args = array( 'post_type' => 'product', 'posts_per_page' => 4);?>
+                         <?php $loop = new WP_Query( $args ); ?>
+                         <?php while ( $loop->have_posts() ) : $loop->the_post();  if ( get_field( 'hottest' ) == 1 ): global $product; ?>
                     <div class="">
-                        <div class="miniaturas" style="background:url(https://images-eu.ssl-images-amazon.com/images/I/61VAMjUPyhL.jpg)">
-
-                        </div>
+                        <div class="miniaturas" style="background:url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
                     </div>
-                    <!--end miiniaturas -->
-
-                    <!-- miiniaturas -->
-                    <div class="content-mini">
-                        <div class="miniaturas" style="background:url(https://files.merca20.com/uploads/2017/03/Lumidoll.png)">
-
-                        </div>
-                    </div>
-                    <!--end miiniaturas -->
-                    <!-- miiniaturas -->
-                    <div class="">
-                        <div class="miniaturas" style="background:url(https://eldiariony.files.wordpress.com/2017/03/aki-dolls1a1.jpg?quality=80&strip=all&strip=all)">
-
-
-                        </div>
-                    </div>
-                    <!--end miiniaturas -->
-
-
-                    <!-- miiniaturas -->
-                    <div class="">
-                        <div class="miniaturas" style="background:url(https://images-na.ssl-images-amazon.com/images/I/51fGn99YTOL._SY550_.jpg)">
-
-                        </div>
-                    </div>
-                    <!--end miiniaturas -->
-
-                    <!--end miiniaturas -->
-
+                         <?php endif; endwhile; ?>
+                    
 
                 </div>
             </div>
         </div>
     </section>
+
