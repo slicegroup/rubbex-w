ac406033ae7774013fc083f73a2c7b8809816c39
Yonnys Quintero <leafar.qr@gmail.com>
2019-05-30 12:00:04 -0400

    custom post type advertising

--- wp-content/themes/Rubbex/functions.php
+++ wp-content/themes/Rubbex/functions.php
@@ -128,3 +128,59 @@ function Banner() {
 }
 add_action( 'init', 'Banner', 0 );
 
+// Register Custom Post Type
+function banner_advertising() {
+
+	$labels = array(
+		'name'                  => _x( 'advertising', 'Post Type General Name', 'apk' ),
+		'singular_name'         => _x( 'advertising', 'Post Type Singular Name', 'apk' ),
+		'menu_name'             => __( 'Advertising', 'apk' ),
+		'name_admin_bar'        => __( 'advertising', 'apk' ),
+		'archives'              => __( 'Item Archives', 'apk' ),
+		'attributes'            => __( 'Item Attributes', 'apk' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'apk' ),
+		'all_items'             => __( 'All Items', 'apk' ),
+		'add_new_item'          => __( 'Add New Item', 'apk' ),
+		'add_new'               => __( 'Add New', 'apk' ),
+		'new_item'              => __( 'New Item', 'apk' ),
+		'edit_item'             => __( 'Edit Item', 'apk' ),
+		'update_item'           => __( 'Update Item', 'apk' ),
+		'view_item'             => __( 'View Item', 'apk' ),
+		'view_items'            => __( 'View Items', 'apk' ),
+		'search_items'          => __( 'Search Item', 'apk' ),
+		'not_found'             => __( 'Not found', 'apk' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'apk' ),
+		'featured_image'        => __( 'Featured Image', 'apk' ),
+		'set_featured_image'    => __( 'Set featured image', 'apk' ),
+		'remove_featured_image' => __( 'Remove featured image', 'apk' ),
+		'use_featured_image'    => __( 'Use as featured image', 'apk' ),
+		'insert_into_item'      => __( 'Insert into item', 'apk' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'apk' ),
+		'items_list'            => __( 'Items list', 'apk' ),
+		'items_list_navigation' => __( 'Items list navigation', 'apk' ),
+		'filter_items_list'     => __( 'Filter items list', 'apk' ),
+	);
+	$args = array(
+		'label'                 => __( 'advertising', 'apk' ),
+		'description'           => __( 'Post Type Description', 'apk' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title' ),
+		'taxonomies'            => array(  ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-images-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'advertising', $args );
+
+}
+add_action( 'init', 'banner_advertising', 0 );
