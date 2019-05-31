8dbdda00afe14cbdc7d13dc96cc9121753cb29c5
Yonnys Quintero <leafar.qr@gmail.com>
2019-05-31 17:09:51 -0400

    page custom dolls

---
+++ wp-content/themes/Rubbex/page-custom.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+?>
+<section class="custom-dolls">
+	
+  <?php echo do_shortcode('[product_page id="34"]'); ?>
+</section>
+
+
+<?php get_footer(); ?>
